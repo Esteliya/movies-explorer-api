97c96c5395408a3e5b0cecf5287d248f671ca574
Esteliya <avroradis@gmail.com>
2023-08-12 18:22:45 +0300

    сменили порт на 3001

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const ErrorNotFound = require('./errors/ErrorNotFound');
 const {
   NODE_ENV = 'development',
   DB_PRODUCTION,
-  PORT = 3000,
+  PORT = 3001,
   DB_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb',
 } = process.env;
 
